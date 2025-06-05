e5223d37997ef464bd789eff8f7c79631354aaeb
luciano <paralucky23@gmail.com>
2025-06-05 09:24:08 -0300

    termino lka estructura basica y la clase car esta cerca de terminarse solo falta que se muestre el sprite y que no se salga de la pantalla

--- auto_esquivador.py
+++ auto_esquivador.py
@@ -6,17 +6,70 @@ HEIGHT = 600
 BLACK = (0,0,0)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+background = pygame.image.load("assets/Gemini_Generated_Image_43m9ik43m9ik43m91.png").convert()
+class Car(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/auto 0.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.bottom = HEIGHT - 10
+        self.speed_x = 0
+    def update(self):
+        self.speed_x = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speed_x = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speed_x = +5
+        self.rect.x += self.speed_x
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+all_sprites = pygame.sprite.Group()
+car = Car()
+all_sprites.add(car)
 
 
 running = True
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(BLACK)
+
+    all_sprites.update()
+
+
+
+
+
+
+    screen.blit(background,[0,0])
+    pygame.display.flip()
+
+
 
 pygame.quit()
 
