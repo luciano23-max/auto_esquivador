bbe8bfaca5ec82059150a762d6f1c8268ae2d563
luciano <paralucky23@gmail.com>
2025-06-12 09:18:38 -0300

    haciendo nuevos sprites y la clase ostacle

--- auto_esquivador.py
+++ auto_esquivador.py
@@ -42,7 +42,24 @@ class Car(pygame.sprite.Sprite):
             self.rect.left = 170
 
 
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = random.choice(obstacle_images)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH -self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1 , 10)
+        self.speedx = random.randrange(-5, 5)
 
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT +10 or self.rect.left < -25 or self.rect.right >WIDTH +22:
+            self.rect.x = random.randrange(WIDTH -self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+            self.speedy = random.randrange(1,8)
 
 
 
